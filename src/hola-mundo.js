f527cb1f689c6b0bfba8492f6c819b02c3efec30
Gustavo Julián <gjuliane@gmail.com>
2021-10-31 21:34:37 -0600

    scheleton

---
+++ src/hola-mundo.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit-element";
+
+class HolaMundo extends LitElement {
+
+    render() {
+        return html``;
+    }
+    
+}
+
+customElements.define('hola-mundo', HolaMundo);
