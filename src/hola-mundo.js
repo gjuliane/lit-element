9b18cede4f80db58ee5b9bb4e4f4c791194c2b52
Gustavo Julián <gjuliane@gmail.com>
2021-10-31 22:09:08 -0600

    properties

--- src/hola-mundo.js
+++ src/hola-mundo.js
@@ -1,10 +1,17 @@
-import { LitElement, html } from "lit-element";
+// import { LitElement, html } from "lit-element";
+import { LitElement, html } from "lit-element/lit-element";
 
 class HolaMundo extends LitElement {
 
+  static get properties(){
+    return {
+      quien: { type: String }
+    }
+  }
+
   render() {
     return html`
-      <p>Hola mundo desde LitElement</p>
+      <p>Hola <b>${this.quien}</b>!! desde LitElement</p>
     `;
   }
 
